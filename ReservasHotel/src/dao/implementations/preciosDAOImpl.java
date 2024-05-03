d0e0586387cac508db80c7d7d19e4cc799ccff07
eduardomesut <eduardomesut@gmail.com>
2024-05-04 00:21:16 +0200

    Hechos todos los implements, faltan cambiars los adds para que no traigan objetos sino variables

--- ReservasHotel/src/dao/implementations/preciosDAOImpl.java
+++ ReservasHotel/src/dao/implementations/preciosDAOImpl.java
@@ -69,13 +69,35 @@ public class preciosDAOImpl implements preciosDAO, AutoCloseable {
     }
 
     @Override
-    public int updatePrecio(precios cambio) throws Exception {
-        return 0;
+    public int updatePrecio(int idPrecio_cambio, double cambio_alto, double cambio_medio, double cambio_bajo) throws Exception {
+        int r = 0;
+        String sql = "UPDATE precios SET temporada_alta = ?, temporada_media = ?, temporada_baja = ? WHERE id = ?;";
+        try (PreparedStatement pst = con.prepareStatement(sql);){
+            pst.setDouble(1,cambio_alto);
+            pst.setDouble(2,cambio_medio);
+            pst.setDouble(3,cambio_bajo);
+            pst.setInt(4,idPrecio_cambio);
+            r = pst.executeUpdate();
+        }catch (Exception e){
+            throw e;
+        }
+        return r;
     }
 
     @Override
-    public int addprecios(precios add) throws Exception {
-        return 0;
+    public int addprecios(double precio_alto, double precio_medio, double precio_bajo, int habitacion_id) throws Exception {
+        int r = 0;
+        String sql = "INSERT INTO precios (temporada_alta, temporada_media, temporada_baja, id_habitacion) VALUES (?, ?, ?, ?);";
+        try (PreparedStatement pst = con.prepareStatement(sql);){
+            pst.setDouble(1,precio_alto);
+            pst.setDouble(2,precio_medio);
+            pst.setDouble(3,precio_bajo);
+            pst.setInt(4,habitacion_id);
+            r = pst.executeUpdate();
+        }catch (Exception e){
+            throw e;
+        }
+        return r;
     }
 
     @Override
--- ReservasHotel/src/dao/interfaces/preciosDAO.java
+++ ReservasHotel/src/dao/interfaces/preciosDAO.java
@@ -9,7 +9,7 @@ public interface preciosDAO {
 
     public precios getPrecio(int id_habitacion, String fecha_entrada) throws Exception;
     public precios getPrecioByReserva(int id_reserva) throws Exception;
-    public int updatePrecio(precios cambio) throws Exception;
-    public int addprecios(precios add)throws Exception;
+    public int updatePrecio(int id_cambio, double cambio_alto, double cambio_medio, double cambio_bajo) throws Exception;
+    public int addprecios(double precio_alto, double precio_medio, double precio_bajo, int habitacion_id)throws Exception;
 
 }
